9bdc15b6273a3095e09eff5008042d6463890a55
Tamar Sinauridze <sin165165@gmail.com>
2022-10-17 01:08:32 +0400

    add search by first or last name

--- src/components/Receiver.js
+++ src/components/Receiver.js
@@ -11,7 +11,77 @@ const Receiver = (props) => {
         return () => clearTimeout(timeOutId);
     }, [receiver]);
 
-    const searchUsers = (term) => {
+    const searchUsers = (searchTerm) => {
+        const term = searchTerm.trim();
+        if(!term) {
+            setList({});
+            setProfilePictureList({});
+            return;
+        }
+        const words = term.split(' ');
+        let url = '';
+        
+        if( +term > 0 ) {
+            url = 'https://www.girchi.com/jsonapi/user/user?' + new URLSearchParams({
+                'include': 'user_picture',
+                'filter[orGroup][group][conjunction]': 'OR',
+                'filter[first][condition][path]': 'drupal_internal__uid',
+                'filter[first][condition][memberOf]': 'orGroup',
+                'filter[first][condition][operator]': '=',
+                'filter[first][condition][value]': term,
+                'filter[last][condition][path]': 'field_personal_id',
+                'filter[last][condition][memberOf]': 'orGroup',
+                'filter[last][condition][operator]': '=',
+                'filter[last][condition][value]': term,
+            });
+        } else if (term && words.length === 1) {
+            url = 'https://www.girchi.com/jsonapi/user/user?' + new URLSearchParams({
+                'include': 'user_picture',
+                'filter[orGroup][group][conjunction]': 'OR',
+                'filter[display][condition][path]': 'name',
+                'filter[display][condition][memberOf]': 'orGroup',
+                'filter[display][condition][operator]': 'CONTAINS',
+                'filter[display][condition][value]': term,
+                'filter[first][condition][path]': 'field_first_name',
+                'filter[first][condition][memberOf]': 'orGroup',
+                'filter[first][condition][operator]': 'STARTS_WITH',
+                'filter[first][condition][value]': term,
+                'filter[last][condition][path]': 'field_last_name',
+                'filter[last][condition][memberOf]': 'orGroup',
+                'filter[last][condition][operator]': 'STARTS_WITH',
+                'filter[last][condition][value]': term,
+            });
+        } else if (words.length > 1) {
+            url = 'https://www.girchi.com/jsonapi/user/user?' + new URLSearchParams({
+                'include': 'user_picture',
+                'filter[orGroupOne][group][conjunction]': 'OR',
+                'filter[orGroupTwo][group][conjunction]': 'OR',
+                'filter[firstOne][condition][path]': 'name',
+                'filter[firstOne][condition][memberOf]': 'orGroupOne',
+                'filter[firstOne][condition][operator]': 'CONTAINS',
+                'filter[firstOne][condition][value]': words[0],
+                'filter[firstTwo][condition][path]': 'field_first_name',
+                'filter[firstTwo][condition][memberOf]': 'orGroupOne',
+                'filter[firstTwo][condition][operator]': 'STARTS_WITH',
+                'filter[firstTwo][condition][value]': words[0],
+                'filter[lastOne][condition][path]': 'name',
+                'filter[lastOne][condition][memberOf]': 'orGroupTwo',
+                'filter[lastOne][condition][operator]': 'CONTAINS',
+                'filter[lastOne][condition][value]': words[1],
+                'filter[lastTwo][condition][path]': 'field_last_name',
+                'filter[lastTwo][condition][memberOf]': 'orGroupTwo',
+                'filter[lastTwo][condition][operator]': 'STARTS_WITH',
+                'filter[lastTwo][condition][value]': words[1],
+            });
+        }
+
+        if(url) {
+            const retrieved = retrieve(url);
+            retrieved.then(result=>{
+                setList(result.data);
+                setProfilePictureList(result.included);
+            });
+        }
 
         async function retrieve(url) {
             try {
@@ -27,37 +97,12 @@ const Receiver = (props) => {
                 return console.log('API got an error', err);
             }
         }
-
-        let url = '';
-        if(typeof +term === 'number') {
-            url = 'https://www.girchi.com/jsonapi/user/user?' + new URLSearchParams({
-                'include': 'user_picture',
-                'filter[orGroup][group][conjunction]': 'OR',
-                'filter[first][condition][path]': 'drupal_internal__uid',
-                'filter[first][condition][memberOf]': 'orGroup',
-                'filter[first][condition][operator]': '=',
-                'filter[first][condition][value]': term,
-                'filter[last][condition][path]': 'field_personal_id',
-                'filter[last][condition][memberOf]': 'orGroup',
-                'filter[last][condition][operator]': '=',
-                'filter[last][condition][value]': term,
-            });
-        } // add search by letters here
-
-        if(url) {
-            const retrieved = retrieve(url);
-            retrieved.then(result=>{
-                setList(result.data);
-                setProfilePictureList(result.included);
-            });
-        }
-
     }
 
     return (
         <input
             className="search flex w-full justify-between items-center p-[10px] gap-[8px] bg-white border-solid border border-bgGray rounded-md 
-                    h-11 placeholder:font-medium placeholder:text-sm placeholder:leading-4"
+                h-11 placeholder:font-medium placeholder:text-sm placeholder:leading-4"
             type="text"
             placeholder="სახელი-გვარი / პირადი ნომერი / მომხმარებლის ID"
             name="userName"
--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,11 +1,16 @@
 import point from '../images/point.svg';
-import picture from '../images/profile-picture.png'; // need another default picture
 
 const Suggestions = (props) => {
+    const noPicture = 'https://www.girchi.com/sites/default/files/avatars/2022-09/_1009237373362919.jpg';
     const list = props.list;
     const profilePictureList = props.profilePictureList;
     const checkPicture = id => {
-        return 'https://www.girchi.com' + profilePictureList.filter(item => item.id===id)[0].attributes.uri.url;
+        const filtered = profilePictureList.filter(item => item.id===id)[0];
+        if(filtered) {
+            return 'https://www.girchi.com' + filtered.attributes.uri.url;
+        } else {
+            return noPicture;
+        }
     }
 
     return (
@@ -13,7 +18,7 @@ const Suggestions = (props) => {
             <div className=" absolute overflow-scroll overflow-x-hidden flex-col items-center p-1.5  w-full  max-h-[284px] bg-white border-solid border-[1px] border-[#F2F2F2] rounded-md " >
                 { list && list.map(item => (
                     <div key={item.id} className="flex flex-row items-center px-[12px] py-[8px] gap-3 w-full min-h-[68px]  rounded-[4px] cursor-pointer ">
-                        <img className="w-10 h-10" src={ item.relationships.user_picture.data ? checkPicture(item.relationships.user_picture.data.id) : picture } />
+                        <img className="w-10 h-10 rounded-full" src={ item.relationships && item.relationships.user_picture.data ? checkPicture(item.relationships.user_picture.data.id) : noPicture } />
                         <div className="flex flex-col">
                             <h4 className="font-normal text-[14px] leading-6 text-[#292D33] ">
                                 { item.attributes.field_first_name } { item.attributes.field_last_name }
